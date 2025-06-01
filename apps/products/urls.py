5065d283f24b036bef8459e12b13a522672c8fd9
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-06-01 17:31:32 +0100

    Create css/js folders and files. Add view and configure url for bundle_list_view. Add bundle.html template to products sub-folder.

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,10 +1,11 @@
 # apps/products/urls.py
 
 from django.urls import path
-from .views import product_list_view
+from .views import product_list_view, bundle_list_view
 
 app_name = 'products'
 
 urlpatterns = [
     path('', product_list_view, name='product_list'),
+    path('bundles/', bundle_list_view, name='bundles'),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,9 +1,14 @@
 # apps/products/views.py
 
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Bundle
 
 
 def product_list_view(request):
     products = Product.objects.all()
     return render(request, 'products/product_list.html', {'products': products})
+
+
+def bundle_list_view(request):
+    bundles = Bundle.objects.all()
+    return render(request, 'products/bundle_list.html', {'bundles': bundles})
