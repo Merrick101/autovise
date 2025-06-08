e6659bc320257836e352fdeea6bf06becc40a9f6
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-06-08 20:58:50 +0100

    Correct minor issue in users/urls by including app_name at top of file

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,6 +3,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('save/<int:product_id>/', views.save_product, name='save_product'),
